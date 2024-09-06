4f19af8f7b0eb7794dbcdb1555ee770b4e6131fe
carolinx <carolinx@umich.edu>
2024-09-06 19:00:17 -0400

    didn't push all changes from LCD interface???????

--- lab2/LCD_Interface.cpp
+++ lab2/LCD_Interface.cpp
@@ -30,6 +30,9 @@ class LCD{
 };
 
 byte LCD::get_status(){
+  /*
+    Get the busy bit and the current AC
+  */
   for(int i = 0; i < 8; ++i){
     pinMode(DBPinNum_t[i], INPUT);
   }
@@ -44,32 +47,44 @@ byte LCD::get_status(){
 }
 
 void LCD::write_line(char msg[], int line, int position){
+  /*
+    Write given string to the specified line starting at position.
+    The spaces that don't have chars are left blank.
+  */
   assert(line <= (numLines_t + 1));
   assert(position < 16);
 
   int write_addr = (line - 1) * 64 + position;
-  Serial.print("Write addr: ");
-  Serial.println(write_addr, HEX);
+  // Serial.print("Write addr: ");
+  // Serial.println(write_addr, HEX);
+
   byte regData = 128 | write_addr;
   write_reg(regData, 0);
-  size_t str_len = strlen(msg); // will this include the null char \0 uhhhh only one way to find out :D xD
-  Serial.print("String Length: ");
-  Serial.println(str_len);
+
+  size_t str_len = strlen(msg);
+  // Serial.print("String Length: ");
+  // Serial.println(str_len);
+
   for(int i = 0; i < ((int)str_len); i++){
     write_reg((int) msg[i], 1);
-    Serial.print("Printing ");
-    Serial.println(msg[i]);
-    /*if (write_addr == 0x0F){
+    // Serial.print("Printing ");
+    // Serial.println(msg[i]);
+
+    // update address correctly when it needs to wrap around
+    if (write_addr == 0x0F){
       write_addr = 0x40;
       byte regWrite = 128 | 0x40;
       write_reg(regWrite, 0);
-    } else if (write_addr == 0x4F){
+    }
+    else if (write_addr == 0x4F){
       write_addr = 0x00;
       byte regWrite = 128 | 0x00;
       write_reg(regWrite, 0);
-    }*/
+    }
+
     delay(25);
   }
+
   for(int i = str_len; i < 16; i++){
     write_reg((int) blank, 1);
     delay(25);
@@ -78,6 +93,9 @@ void LCD::write_line(char msg[], int line, int position){
 }
 
 void LCD::write_byte(char character, int line, int position){
+  /*
+    Write a single character onto the LCD at the specified line and position
+  */
   int write_addr = (line - 1) * 64 + position;
   byte regData = 128 | 
                  (bitRead(write_addr, 6)) | 
@@ -92,10 +110,14 @@ void LCD::write_byte(char character, int line, int position){
 }
 
 void LCD::set_pins(int RSPinNum, int RWPinNum, int EnablePinNum, int DBPinNum[]) {
-
+  /*
+    Sets the pin numbers on the Arduino to the correct mode and 
+    connects them to the corresponding pins on the LCD.
+  */
   RSPinNum_t = RSPinNum;
   RWPinNum_t = RWPinNum;
   EnablePinNum_t = EnablePinNum;
+
   for(int i = 0; i < 8; ++i) {
     DBPinNum_t[i] = DBPinNum[i];
   }
@@ -103,6 +125,7 @@ void LCD::set_pins(int RSPinNum, int RWPinNum, int EnablePinNum, int DBPinNum[])
   pinMode(RSPinNum_t, OUTPUT);  
   pinMode(RWPinNum_t, OUTPUT);
   pinMode(EnablePinNum_t, OUTPUT);
+
   for(int i = 0; i < 8; ++i) {
     pinMode(DBPinNum_t[i], OUTPUT);
   }
@@ -115,7 +138,6 @@ void LCD::screen_init(int numBits, int numLines, int fontType){
     entry mode set
     turn on display
   */
- //Serial.println("Inside Screen Init");
   assert((numBits == 4) || (numBits == 8));
   assert((numLines == 2) || (numLines == 1));
   assert((fontType == 1) || (fontType == 0));
@@ -126,15 +148,18 @@ void LCD::screen_init(int numBits, int numLines, int fontType){
   
   byte regData = (1 << 5) | (numBits_t << 4) | (numLines_t << 3) | (fontType_t << 2);
 
-  Serial.print("regData: ");
-  Serial.println(regData, BIN);
+  // Serial.print("regData: ");
+  // Serial.println(regData, BIN);
+  write_reg(regData, 0);
+  delay(1);
 
+  // Set entry mode
+  regData = 0b00000110;
   write_reg(regData, 0);
-  regData = 0b00000110; //entry mode 
   delay(1);
-  write_reg(regData, 0);
-  delay(1);
-  regData = 0b00001110; //screen on
+
+  // Turn on display
+  regData = 0b00001110;
   write_reg(regData, 0);
 }
 
@@ -151,9 +176,11 @@ void LCD::write_reg(byte regData, boolean regSelect){
   digitalWrite(RSPinNum_t, regSelect);
   digitalWrite(RWPinNum_t, 0);
   delay(1);
+
   digitalWrite(EnablePinNum_t, 1);
   delay(1);
-  if (numBits_t == 0){
+
+  if (numBits_t == 0){ // 4-bit mode
     for (int i = 4; i < 8; i++){
       digitalWrite(DBPinNum_t[i], bitRead(regData, i));
     }
@@ -168,7 +195,8 @@ void LCD::write_reg(byte regData, boolean regSelect){
     delay(1);
     digitalWrite(EnablePinNum_t, 0);
     delay(1);
-  } else {
+  } 
+  else { // if numBits_t == 1 (8-bit mode)
     for (int i = 0; i < 8; i++){
       digitalWrite(DBPinNum_t[i], bitRead(regData, i));
     }
@@ -179,19 +207,27 @@ void LCD::write_reg(byte regData, boolean regSelect){
 }
 
 byte LCD::read_reg(boolean regSelect){
+  /*
+    Read value from registers.
+    If regSelect is 0, read from IR
+    If regSelect is 1, read from DR
+  */
   digitalWrite(RSPinNum_t, regSelect);
   digitalWrite(RWPinNum_t, 1);
   delay(1);
   digitalWrite(EnablePinNum_t, 1);
   delay(1);
+
   byte readData;
   for(int i = 0; i < 8; i++){
     int data = digitalRead(DBPinNum_t[i]);
     bitWrite(readData, i, data);
   }
+
   return readData;
 }
 
+// Initialize global LCD class
 LCD Display1;
 
 void setup() {
@@ -223,14 +259,11 @@ void setup() {
   delay(100);
   //byte debugReturn = Display1.read_reg(0);
   //Serial.print("Address: ");
-  //Serial.println(debugReturn, BIN);
-  Serial.println("Leaving void setup");
-  
+  //Serial.println(debugReturn, BIN);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //Serial.println("In Void Loop");
   Display1.write_line(roombaString, 2, 0);
   delay(2000);
   Display1.write_line(BLEString, 2, 0);
