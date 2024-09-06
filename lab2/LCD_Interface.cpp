8b9919684d8f8c96667d0e8c6ce007bdffe4a895
carolinx <carolinx@umich.edu>
2024-09-06 18:58:51 -0400

    LCD Interface commented now

--- lab2/LCD_Interface.cpp
+++ lab2/LCD_Interface.cpp
@@ -1,33 +1,32 @@
-    #include <assert.h>
-    
-    
-    
-    int RSPinNum_t;
-    int RWPinNum_t;
-    uint8_t DBPinNum_t[8];
-    int EnablePinNum_t;
-    char roombaString[] = "Roomba!";
-    char BLEString[] = "BLE 4ever";
-    char blank = ' ';
+#include <assert.h>
+
+
+
+int RSPinNum_t;
+int RWPinNum_t;
+uint8_t DBPinNum_t[8];
+int EnablePinNum_t;
+char roombaString[] = "Roomba!";
+char BLEString[] = "BLE 4ever";
+char blank = ' ';
     
 class LCD{
-    String cur_msg;
-    int cur_addr; // top left corner address
-    int numBits_t;
-    int numLines_t;
-    int fontType_t;
-
-
-    byte get_status();
-
-    public:
-        void clear_screen();
-        void write_line(char msg[], int line, int position);
-        void write_reg(byte regData, boolean regSelect); //Set regSelect to 0 for IR, 1 for DR
-        byte read_reg(boolean regSelect); //Set regSelect to 0 for IR, 1 for DR
-        void write_byte(char character, int line, int position);
-        void set_pins(int RSPinNum, int RWPinNum, int EnablePinNum, int DBPinNum[]);
-        void screen_init(int numBits, int numLines, int fontType);
+  String cur_msg;
+  int cur_addr; // top left corner address
+  int numBits_t;
+  int numLines_t;
+  int fontType_t;
+  
+  byte get_status();
+  
+  public:
+    void clear_screen();
+    void write_line(char msg[], int line, int position);
+    void write_reg(byte regData, boolean regSelect); //Set regSelect to 0 for IR, 1 for DR
+    byte read_reg(boolean regSelect); //Set regSelect to 0 for IR, 1 for DR
+    void write_byte(char character, int line, int position);
+    void set_pins(int RSPinNum, int RWPinNum, int EnablePinNum, int DBPinNum[]);
+    void screen_init(int numBits, int numLines, int fontType);
 };
 
 byte LCD::get_status(){
