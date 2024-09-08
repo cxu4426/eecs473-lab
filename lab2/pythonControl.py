71bd41cc53cfffd898dfd16111cce169ca1fc8e9
yangje <yangje@umich.edu>
2024-09-08 00:18:44 -0400

    Added speed commands to Arduino code

--- lab2/pythonControl.py
+++ lab2/pythonControl.py
@@ -5,13 +5,17 @@ while 1:
     # Poll keyboard
     if msvcrt.kbhit():
         key = msvcrt.getch()
-        if key == b'f':
+        if key == b'w':
             ser.write(str.encode('C21FE'))
-        elif key == b's':
+        elif key == b' ':
             ser.write(str.encode('C21SE'))
-        elif key == b'l':
+        elif key == b'a':
             ser.write(str.encode('C21LE'))
-        elif key == b'r':
+        elif key == b'd':
             ser.write(str.encode('C21RE'))
-        elif key == b'b':
+        elif key == b's':
             ser.write(str.encode('C21BE'))
+        elif key == b'+':
+            ser.write(str.encode('C21+E'))
+        elif key == b'-':
+            ser.write(str.encode('C21-E'))
